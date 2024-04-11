d75589407e3d2dd36f316e4ef5f2134c1cf8a26e
Jeffrey King <jking@hey.com>
2024-04-11 15:02:30 +0800

    add first graph

---
+++ src/lib/features/tripsByDay/TripsByDayChart.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import * as Plot from '@observablehq/plot';
+	import { type GetQuery } from '$lib/types/queries';
+	import { onMount } from 'svelte';
+	import type { RawTableRow, Day } from './types';
+	import { tripsByDayQuery } from './constants';
+	export let getQuery: GetQuery;
+	let container: HTMLDivElement;
+
+	let tripsData = [];
+
+	onMount(async () => {
+		const table = await getQuery(tripsByDayQuery);
+		const table_arr = table.toArray(); // list of objects, compatible with OJS
+
+		const tripsData = table_arr.map((row: RawTableRow) => {
+			return {
+				dayIndex: Number(row.day),
+				dayName: row.dayname,
+				trips: Number(row.trips)
+			};
+		});
+
+		const plot = Plot.plot({
+			marginLeft: 90,
+			y: { label: null },
+			marks: [
+				Plot.barX(tripsData, {
+					x: 'trips',
+					y: 'dayName',
+					sort: {
+						y: 'data',
+						reduce: ([row]: Day[]) => {
+							return row.dayIndex;
+						}
+					}
+				}),
+				Plot.text(tripsData, {
+					text: (d) => d.trips,
+					x: 'trips',
+					y: 'dayName',
+					textAnchor: 'end',
+					dx: -3,
+					fill: 'white'
+				})
+			]
+		});
+
+		container.append(plot); // Append the plot to our container
+	});
+</script>
+
+{#if tripsData.length === 0}
+	<div>Loading Bar Data</div>
+{/if}
+<div bind:this={container} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import TripsByDayChart from '$lib/features/tripsByDay/TripsByDayChart.svelte';
+
+	export let data;
+</script>
+
+<h1>Bluebike data</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<TripsByDayChart getQuery={data.getQuery} />
