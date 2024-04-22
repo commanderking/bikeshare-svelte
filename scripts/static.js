e001d12f40142b05b378553ee065ef11f3285759
Jeffrey King <jking@hey.com>
2024-04-22 21:57:39 +0800

    setup script to download static file for vercel build

---
+++ scripts/static.js
@@ -0,0 +1,35 @@
+// scripts/download-from-s3-public.js
+
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+
+const url =
+	'https://city-bike-share.s3.ap-southeast-2.amazonaws.com/all_trips_february_2024.parquet';
+const outputPath = path.join('./static/local', 'all_trips.parquet');
+
+const downloadFile = async () => {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) throw new Error(`Failed to fetch file: ${response.statusText}`);
+
+		const fileStream = fs.createWriteStream(outputPath);
+
+		if (!response.body) {
+			return 'No repsonse from file';
+		}
+		response.body.pipe(fileStream);
+
+		fileStream.on('error', (err) => {
+			console.error('Error writing file', err);
+		});
+
+		fileStream.on('finish', () => {
+			console.log(`File downloaded successfully from S3 to ${outputPath}`);
+		});
+	} catch (err) {
+		console.error('Error downloading file from S3', err);
+	}
+};
+
+downloadFile();
